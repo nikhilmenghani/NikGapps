9dd5034beffe6af613cac61094d5419cd22fe7e7
Nikhil Menghani <nikhil@menghani.com>
2024-08-12 22:36:38 -0400

    Migrate to new method of injecting dependencies

--- app/src/main/java/com/nikgapps/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/nikgapps/ui/screens/SettingsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -25,11 +26,11 @@ fun SettingsScreen(navController: NavHostController, viewModel: SharedViewModel)
 
     Scaffold(
         topBar = {
-            TopAppBar(
+            LargeTopAppBar(
                 title = { Text(text = "Settings") },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 }
             )
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "NikGapps"
 include(":app")
- 
