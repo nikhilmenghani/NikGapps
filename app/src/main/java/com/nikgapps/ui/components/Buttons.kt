a58a8aca1fe41284b9357b7816609f8b5309cdeb
Nikhil Menghani <nikhil@menghani.com>
2024-09-09 11:34:41 -0400

    Add TextButtonWithIcon with generic Preview

--- app/src/main/java/com/nikgapps/ui/components/Buttons.kt
+++ app/src/main/java/com/nikgapps/ui/components/Buttons.kt
@@ -1,20 +1,25 @@
 package com.nikgapps.ui.components
 
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.outlined.Update
+import androidx.compose.material.icons.outlined.Upload
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nikgapps.ui.theme.NikGappsThemePreview
 
 @Composable
 fun OutlinedButtonWithIcon(
@@ -44,11 +49,54 @@ fun OutlinedButtonWithIcon(
 }
 
 @Composable
-@Preview
-fun OutlinedButtonWithIconPreview() {
-    OutlinedButtonWithIcon(
-        onClick = { /* Do something */ },
-        icon = Icons.Default.Favorite, // Use a proper ImageVector like Icons.Default
-        text = "Click Me"
+fun TextButtonWithIcon(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    text: String,
+    contentColor: Color = MaterialTheme.colorScheme.primary,
+    onClick: () -> Unit,
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+        colors = ButtonDefaults.textButtonColors(contentColor = contentColor)
     )
+    {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.size(18.dp),
+                imageVector = icon,
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = text
+            )
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun Preview() {
+    NikGappsThemePreview() {
+        Surface {
+            Column {
+                PreferenceSubtitle(text = "Preview")
+                OutlinedButtonWithIcon(
+                    onClick = { /* Do something */ },
+                    icon = Icons.Default.Favorite,
+                    text = "Click Me"
+                )
+//                Spacer(modifier = Modifier.height(8.dp))
+                TextButtonWithIcon(
+                    onClick = { /* Do something */ },
+                    icon = Icons.Default.FavoriteBorder,
+                    text = "Click Me"
+                )
+            }
+        }
+    }
 }
