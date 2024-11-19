1d09a3be90f0fdedbcb25ee5bc1f9df1643abe05
Nikhil Menghani <nikhil@menghani.com>
2024-11-19 17:58:44 -0500

    Add the ability to preview on multiple devices

--- app/src/main/java/com/nikgapps/app/presentation/ui/component/cards/RootAccessCard.kt
+++ app/src/main/java/com/nikgapps/app/presentation/ui/component/cards/RootAccessCard.kt
@@ -70,7 +70,10 @@ fun RootAccessCard() {
     }
 }
 
-@Preview
+@Preview(name = "phone", device = "spec:width=360dp,height=640dp,dpi=480")
+@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
+@Preview(name = "foldable", device = "spec:width=673dp,height=841dp,dpi=480")
+@Preview(name = "tablet", device = "spec:width=1280dp,height=800dp,dpi=480")
 @Composable
 fun PreviewGetRootAccessCard() {
     RootAccessCard()
