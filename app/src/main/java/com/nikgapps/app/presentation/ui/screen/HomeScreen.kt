c0b3e78ff976c05cc443b138e337752aa5326513
Nikhil Menghani <nikhil@menghani.com>
2024-11-19 10:35:09 -0500

    Remove RequestNotificationPermission from usages

--- app/src/main/java/com/nikgapps/app/presentation/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/nikgapps/app/presentation/ui/screen/HomeScreen.kt
@@ -1,27 +1,33 @@
 package com.nikgapps.app.presentation.ui.screen
 
-import ModalBottomSheetM3Example
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.nikgapps.MainActivity
 import com.nikgapps.app.presentation.navigation.Screens
-import com.nikgapps.app.utils.extensions.navigateWithState
-import com.nikgapps.app.presentation.ui.component.buttons.CopyFileButton
-import com.nikgapps.app.presentation.ui.component.buttons.DownloadAndExtractButton
-import com.nikgapps.app.presentation.ui.component.buttons.DownloadButton
-import com.nikgapps.app.presentation.ui.component.buttons.RequestNotificationPermission
 import com.nikgapps.app.presentation.ui.component.cards.GetRootAccessCard
 import com.nikgapps.app.presentation.ui.component.cards.UpdateAppCard
-import com.nikgapps.app.presentation.ui.component.common.RootStatusDisplay
 import com.nikgapps.app.presentation.ui.component.dialogs.SettingsDialog
-import com.nikgapps.MainActivity
+import com.nikgapps.app.utils.extensions.navigateWithState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
