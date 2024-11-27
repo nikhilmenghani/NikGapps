7ea155ffd7e929c27369880385891dadd3213f1e
Nikhil Menghani <nikhil@menghani.com>
2024-11-26 22:01:33 -0500

    Optimize the code

--- app/src/main/java/com/nikgapps/app/utils/root/RootManager.kt
+++ app/src/main/java/com/nikgapps/app/utils/root/RootManager.kt
@@ -7,23 +7,17 @@ data class ScriptResult(val success: Boolean, val output: String)
 
 class RootManager(private val context: Context) {
 
-    var rootAccess: Boolean = false
-    var command: String = ""
-    var sourcePath: String = ""
-    var destPath: String = ""
-
     fun executeScript(scriptId: Int, vararg args: String): ScriptResult {
         val resourceName = context.resources.getResourceEntryName(scriptId)
         val scriptFile = File(context.filesDir, "$resourceName.sh")
         val inputStream = context.resources.openRawResource(scriptId)
-        val scriptPath = scriptFile.absolutePath
         return try {
             scriptFile.outputStream().use { outputStream ->
                 inputStream.copyTo(outputStream)
             }
-            val process = Runtime.getRuntime().exec("su -c chmod +x $scriptPath")
+            val process = Runtime.getRuntime().exec("su -c chmod +x $scriptFile.absolutePath")
             process.waitFor()
-            val result = executeScript(scriptPath, *args)
+            val result = executeScript(scriptFile.absolutePath, *args)
             ScriptResult(true, result)
         } catch (e: Exception) {
             e.printStackTrace()
